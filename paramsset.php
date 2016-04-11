fd9f2f311dc119a06281d763f033ec724965d214
Ievgen Kuts <kyts@users.noreply.github.com>
2016-04-11 14:23:21 +0300

    Update paramsset.php

--- paramsset.php
+++ paramsset.php
@@ -1,20 +1,21 @@
 <?php
-require_once("classes/MyDB.class.php");
-require_once("classes/Params.class.php");
-require_once('classes/functions.php');
+/**
+* Setting parameters
+*/
+function __autoload( $className ) {
+  $className = str_replace( "..", "", $className );
+  require_once( "classes/$className.class.php" );
+}
 
 $db = new MyDB('zak.sqlite');
 if(!$db){
     echo $db->lastErrorMsg();
 } else {
-   // echo "Opened database successfully\n";
+   $hz = new Params($db);
+	if ($hz->setParams($_POST)) {
+		echo 'ok';
+	}
 }
  
-$hz = new Params($db);
-
-if ($hz->setParams($_POST)) {
-	echo 'ok';
-}
-
 $db->close();
 ?>
