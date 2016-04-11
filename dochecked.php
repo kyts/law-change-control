e2bbb4a32df202092aab6dc3212304061e0eb8bf
Ievgen Kuts <kyts@users.noreply.github.com>
2016-04-11 12:36:41 +0300

    Update dochecked.php

--- dochecked.php
+++ dochecked.php
@@ -1,22 +1,25 @@
 <?php
-require_once("classes/MyDB.class.php");
-require_once("classes/classModifyHandZminy.php");
-require_once('classes/functions.php');
+function __autoload( $className ) {
+  $className = str_replace( "..", "", $className );
+  require_once( "classes/$className.class.php" );
+}
+require_once("classes/functions.php");
 
 $checked_doc = $_POST["code"];
  
-print_r($checked_doc);
-
 $db = new MyDB('zak.sqlite');
+
 if(!$db){
     echo $db->lastErrorMsg();
 } else {
-   // echo "Opened database successfully\n";
+    $hz = new ModifyHandZminy($db);
+    $modified = $hz->toggleChecked($checked_doc);
+    if ($modified) {
+	echo "OK";
+    } else {
+	echo "FALSE";
+    }
 }
- 
-$hz = new ModifyHandZminy($db);
-
-echo $hz->setChecked($checked_doc);
 
 $db->close();
 ?>
